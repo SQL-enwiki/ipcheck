c0d7c6671b391c5fa851376f1ab606cdddc0c23d
SQL-enwiki <45909509+SQL-enwiki@users.noreply.github.com>
2019-01-26 18:15:54 -0500

    Add CheckHost google/amazon/azure detection

---
+++ checkhost/checkhost.php
@@ -0,0 +1,72 @@
+<?php
+
+//Match an IPv4 IP to a CIDR
+function cidr_match( $ip, $cidr ) {
+    list( $subnet, $mask ) = explode( '/', $cidr );
+    if ( ( ip2long( $ip ) & ~( ( 1 << ( 32 - $mask ) ) - 1 ) ) == ip2long( $subnet ) ) { 
+        return 1;
+    }
+    return 0;
+}
+
+function inet_to_bits( $inet ) {
+   @$unpacked = unpack( 'A16', $inet );
+   $unpacked = str_split( $unpacked[1] );
+   $binaryip = '';
+   foreach ( $unpacked as $char ) {
+             $binaryip .= str_pad( decbin( ord( $char ) ), 8, '0', STR_PAD_LEFT );
+   }
+   return $binaryip;
+}    
+
+//Match an IPv4 IP to a CIDR
+function cidr_match6( $ip, $cidr ) {
+	$ip = inet_pton( $ip );
+	$binaryip = inet_to_bits( $ip );
+	list( $net, $maskbits )=explode( '/',$cidr );
+	$net = inet_pton( $net );
+	$binarynet = inet_to_bits( $net );
+	$ip_net_bits = substr( $binaryip, 0, $maskbits );
+	$net_bits = substr( $binarynet, 0, $maskbits );
+	if( $ip_net_bits !== $net_bits ) { 
+		return( FALSE );
+	} else {
+		return( TRUE );
+	}
+}
+
+//Check to see if the host is in the compute JSON list
+function checkCompute( $ip ) {
+	$ranges = json_decode( file_get_contents( "/data/project/ipcheck/whsrc/computehosts.json" ), TRUE );
+	$match = FALSE;
+	foreach( $ranges as $range ) {
+		$r = $range['range'];
+		if( strpos( $r, ":" ) === FALSE ) {
+			//ipv4
+			if( cidr_match( $ip, $r ) ) { $match = TRUE; return( $range ); }
+		} else {
+			//ipv6
+			if( cidr_match6( $ip, $r ) ) { $match = TRUE; return( $range ); }
+			
+		}
+	}
+	if( $match === FALSE ) { return( FALSE ); }
+}
+
+//Split IPv4 ranges larger than /16's into /16's
+function splitrange( $range ) {
+	if( strpos( $range, ":" ) !== FALSE ) { return array( $range ); }
+    $range = explode( "/", $range );
+	if( $range[1] < 16 ) {
+		$rangeoct = "/" . 16;
+	} else {
+		$rangeoct = "/" . $range[1];
+	}
+    $octet = ip2long( $range[0] ); 
+    $sixteens = pow( 2, (16 - $range[1] ) ) - 1;
+    $out = array();
+    for ( $i = -65536; $i < 65536 * $sixteens; $out[] = ( long2ip( $octet + ( $i += 65536 ) ) ) . $rangeoct );
+    return $out;
+}
+
+?>
---
+++ checkhost/computehosts.php
@@ -0,0 +1,98 @@
+<?php
+include( "../checkhost/checkhost.php" );
+
+//Amazon AWS
+$amazonraw = json_decode( file_get_contents( 'https://ip-ranges.amazonaws.com/ip-ranges.json' ), TRUE );
+$computeranges = array();
+foreach( $amazonraw['prefixes'] as $prefix ) {
+	$range = $prefix['ip_prefix'];
+	$split = splitrange( $range );
+	foreach( $split as $sr ) {
+		$arange = array();
+		$arange['service'] = "amazon";
+		$arange['range'] = $sr;
+		array_push( $computeranges, $arange );
+	}
+}
+foreach( $amazonraw['ipv6_prefixes'] as $prefix ) {
+	$range = $prefix['ipv6_prefix'];
+	$arange = array();
+	$arange['service'] = "amazon";
+	$arange['range'] = $range;
+	array_push( $computeranges, $arange );
+}
+
+//Microsoft Azure
+$first = file_get_contents( "https://www.microsoft.com/en-us/download/confirmation.aspx?id=41653" );
+$first = explode( "\n", $first );
+foreach( $first as $f ) {
+	if( strpos( $f, "download manually" ) !== FALSE ) {
+		preg_match( '/href="(.*?)"/i', $f, $matches );
+		$url = $matches[1];
+		break;
+	}
+}
+$second = file_get_contents( $url );
+$second = explode( "\n", $second );
+foreach( $second as $s ) {
+	$test = preg_match( '/IpRange Subnet="(.*?)" \//', $s, $m );
+	if( $test == 1 ) {
+		$range = $m[1];
+		$split = splitrange( $range );
+		foreach( $split as $sr ) {
+			$arange = array();
+			$arange['service'] = "azure";
+			$arange['range'] = $sr;
+			array_push( $computeranges, $arange );
+		}
+	}
+}
+
+//Google Compute Engine
+$dns = dns_get_record( "_cloud-netblocks.googleusercontent.com", DNS_TXT );
+$exp = explode( " ", $dns[0]['txt'] );
+foreach( $exp as $e ) {
+	if( strpos( $e, "include:" ) !== FALSE ) {
+		$e = substr( $e, 8 );
+		$subdns = dns_get_record( $e, DNS_TXT );
+		$sd = explode( " ", $subdns[0]['txt'] );
+		//There are some google cloud domains that aren't listed 35.240.0.0/13, 35.224.0.0/12, 35.208.0.0/12, 35.192.0.0/12
+		array_push( $sd, "ip4:35.240.0.0/13" );
+		array_push( $sd, "ip4:35.224.0.0/12" );
+		array_push( $sd, "ip4:35.208.0.0/12" );
+		array_push( $sd, "ip4:35.192.0.0/12" );
+		//And, IPv6: 
+		array_push( $sd, "ip6:2600:1900::/32" );
+		array_push( $sd, "ip6:2600:1901::/32" );
+		array_push( $sd, "ip6:2600:1902::/32" );
+		array_push( $sd, "ip6:2600:1903::/32" );
+		array_push( $sd, "ip6:2600:1904::/32" );
+		array_push( $sd, "ip6:2600:1905::/32" );
+		array_push( $sd, "ip6:2600:1906::/32" );
+		array_push( $sd, "ip6:2600:1907::/32" );
+		array_push( $sd, "ip6:2600:1908::/32" );
+		array_push( $sd, "ip6:2600:1909::/32" );
+		array_push( $sd, "ip6:2600:190a::/32" );
+		array_push( $sd, "ip6:2600:190b::/32" );
+		array_push( $sd, "ip6:2600:190c::/32" );
+		array_push( $sd, "ip6:2600:190d::/32" );
+		array_push( $sd, "ip6:2600:190e::/32" );
+		array_push( $sd, "ip6:2600:190f::/32" );
+		foreach( $sd as $s ) {
+			$firsttwo = substr( $s, 0, 2 );
+			if( $firsttwo == "ip" ) {
+				$range = substr( $s, 4 );
+				$srange = splitrange( $range );
+				foreach( $srange as $sr ) {
+					$grange = array();
+					$grange['service'] = "google";
+					$grange['range'] = $sr;
+					array_push( $computeranges, $grange );
+				}
+			}
+		}
+	}
+}
+$computeranges = array_map("unserialize", array_unique(array_map("serialize", $computeranges)));
+file_put_contents( "/data/project/ipcheck/whsrc/computehosts.json", json_encode( $computeranges ) );
+?>
