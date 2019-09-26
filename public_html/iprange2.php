04035a045ebf38e719dbc105100ae3cc80d12408
SQL-enwiki <45909509+SQL-enwiki@users.noreply.github.com>
2019-09-26 17:46:27 -0400

    fix dir

--- public_html/iprange2.php
+++ public_html/iprange2.php
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
-require __DIR__ . '/vendor/autoload.php'; 
+require __DIR__ . '/../vendor/autoload.php'; 
 use Amp\Parallel\Worker;
 use Amp\Promise;
 function cidrToRange($value) {
