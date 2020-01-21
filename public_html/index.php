5962a81dbb8a742a2931ade89e810bec95dda3f8
SQL-enwiki <45909509+SQL-enwiki@users.noreply.github.com>
2020-01-21 11:28:24 -0500

    Update IPQS config

--- public_html/index.php
+++ public_html/index.php
@@ -188,7 +188,7 @@ function reportHit( $service ) {
 	$lservice['getipintel'] = array( 'type' => 'min', 'limit' => 15, 'type2' => 'day', 'limit2' => 500 );
 	$lservice['iphub'] = array( 'type' => 'day', 'limit' => 1000, 'type2' => 'day', 'limit2' => 1000 );
 	$lservice['iphunter'] = array( 'type' => 'day', 'limit' => 1000, 'type2' => 'day', 'limit2' => 1000 );
-	$lservice['ipqs'] = array( 'type' => 'month', 'limit' => 5000, 'type2' => 'month', 'limit2' => 5000 );
+	$lservice['ipqs'] = array( 'type' => 'month', 'limit' => 50000, 'type2' => 'month', 'limit2' => 50000 );
 	/* $lservice['nofraud'] = array( 'type' => 'day', 'limit' => 600, 'type2' => 'day', 'limit2' => 600 ); */
 	$lservice['proxycheck-io'] = array( 'type' => 'day', 'limit' => 1000, 'type2' => 'day', 'limit2' => 1000 );
 	$lservice['sorbs'] = array( 'type' => 'min', 'limit' => 1000, 'type2' => 'min', 'limit2' => 1000 );
